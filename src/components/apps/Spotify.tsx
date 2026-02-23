c064992286297c81dba81e4fc379bb182150044a
Robu9 <anushayjain7622@gmail.com>
2026-02-23 15:31:09 +0530

    spotify

--- src/components/apps/Spotify.tsx
+++ src/components/apps/Spotify.tsx
@@ -5,11 +5,11 @@ export const Spotify = () => {
     return (
         <div className="w-full h-full bg-[#121212]">
             <iframe
-                src="https://open.spotify.com/embed/playlist/1uOqV3FneZg4oZfXEDrC66"
+                src="https://open.spotify.com/embed/track/3CLSHJv5aUROAN2vfOyCOh?utm_source=generator&theme=0"
                 width="100%"
                 height="100%"
                 frameBorder="0"
-                allowFullScreen
+                loading="lazy"
                 allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
                 className="w-full h-full"
             />
